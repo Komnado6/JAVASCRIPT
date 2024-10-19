fd1c9c969a2d43142d2304b3ed33f21ea9bbc29a
Komnado6 <osafok299@gmail.com>
2024-10-19 12:57:56 +0000

    Project class

---
+++ l3(details)/Script.js
@@ -0,0 +1,19 @@
+var name = "KOFI" //string
+var age = 70 //number
+var question = false //boolean
+var cars =[]//array
+var pen ={}//object
+var y;//undefined
+var z = null//null
+
+console.log(typeof name)
+var age = String(age)
+console.log(typeof age)
+
+try{
+    //the block of code to test for errors
+ rest()
+}
+catch(error){
+    console.log(error.message)
+}
